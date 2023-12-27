82ee483cf2b53668dcd7dec84190f985775652dc
JooHui-void <elis100@naver.com>
2023-12-27 22:23:20 +0900

    feat: GlobalFilter 연결

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { setupSwagger } from './configs/setUpSwagger';
+import { GlobalExceptionFilter } from './exception/GlobalException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   const serverConfig = config.get('server');
   const port = serverConfig.port;
   setupSwagger(app);
