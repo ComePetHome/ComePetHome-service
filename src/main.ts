62e2236e2bcf76964d5fa7d79616aacebe7cb0d4
JooHui-void <elis100@naver.com>
2024-01-18 00:29:06 +0900

    fix: dto 에러메세지 출력되게 수정

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 import { setupSwagger } from './configs/setUpSwagger';
 import { GlobalExceptionFilter } from './exception/GlobalException.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   const serverConfig = config.get('server');
