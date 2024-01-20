28233ae7a5422de692fca2ae188d97ac0940ded9
JooHui-void <elis100@naver.com>
2024-01-21 05:13:27 +0900

    feat: 이미지 삭제 함수 추가

--- src/imageupload/imageupload.service.ts
+++ src/imageupload/imageupload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
-import * as multerS3 from 'multer-s3';
+
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -23,20 +23,38 @@ export class ImageuploadService {
   }
 
   async fileUpload(file: Express.Multer.File): Promise<string> {
-    const key = `${Date.now() + file.originalname}`;
+    if (file == undefined) {
+      return null;
+    } else {
+      const key = `${Date.now() + file.originalname}`;
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        ACL: 'private',
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ContentDisposition: 'inline',
+      };
+
+      return new Promise((resolve, reject) => {
+        this.s3.putObject(params, (err, data) => {
+          if (err) reject(err);
+          resolve(key);
+        });
+      });
+    }
+  }
+
+  async fileDelete(fileName: string): Promise<void> {
     const params = {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      ACL: 'private',
-      Key: key,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-      ContentDisposition: 'inline',
+      Key: fileName,
     };
 
     return new Promise((resolve, reject) => {
-      this.s3.putObject(params, (err, data) => {
+      this.s3.deleteObject(params, (err, data) => {
         if (err) reject(err);
-        resolve(key);
+        resolve();
       });
     });
   }
